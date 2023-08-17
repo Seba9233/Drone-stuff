954cfc604dd98244d55504938baf66913257a4ed
sebai <sebai9233@gmail.com>
2023-08-17 11:04:02 +0200

    Nice cock

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-tom fil
+from djitellopy import Tello
+import time
+
+# Initialize the Tello drone
+tello = Tello()
+
+# Connect to the drone
+tello.connect()
+
+# Print battery percentage
+print("Battery:", tello.get_battery(), "%")
+
+# Take off
+tello.takeoff()
+
+# Fly forward for 100 cm
+tello.move_forward(100)
+
+# Turn clockwise by 90 degrees
+tello.rotate_clockwise(90)
+
+# Land
+tello.land()
+
+# Disconnect from the drone
+tello.end()
