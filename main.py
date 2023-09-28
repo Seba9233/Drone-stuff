86f3448f5ea833d28dc891243e042879e3fc4cd5
sebai <sebai9233@gmail.com>
2023-09-28 11:07:00 +0200

    Optag 1 sek

--- main.py
+++ main.py
@@ -1,32 +1,54 @@
-from djitellopy import Tello
+import cv2
+import threading
 import time
+from djitellopy import Tello
 
 # Initialize the Tello drone
 tello = Tello()
-
-# Connect to the drone
 tello.connect()
+tello.streamon()
 
-# Print battery percentage
-print("Battery:", tello.get_battery(), "%")
-
-# Take off
 tello.takeoff()
 
-# Fly forward for 100 cm
-tello.move_forward(100)
-time.sleep(1)
+# Create a VideoWriter object to save the video
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, (960, 720))
 
-tello.move_down(20)
+# Function to receive and record video
+def receive_video():
+    start_time = time.time()
+    while True:
+        try:
+            frame = tello.get_frame_read().frame
+            out.write(frame)
+            cv2.imshow('Tello Video', frame)
 
-tello.move_left(20)
+            # Check if 5 seconds have elapsed
+            if time.time() - start_time >= 5:
+                break
 
-tello.move_right(40)
+            # Exit the program if 'q' is pressed
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        except KeyboardInterrupt:
+            break
 
-tello.move_back(100)
+# Start video reception in a separate thread
+video_thread = threading.Thread(target=receive_video)
+video_thread.start()
 
-# Land
+# Hover until 5 seconds have elapsed
+tello.send_rc_control(0, 0, 0, 0)
+time.sleep(5)
+
+# Stop video recording and release resources
+video_thread.join()
+out.release()
+cv2.destroyAllWindows()
+
+# Land the Tello
 tello.land()
 
-# Disconnect from the drone
-tello.end()
+# Disconnect from the Tello
+tello.streamoff()
+tello.disconnect()
